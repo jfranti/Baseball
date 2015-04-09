f4c2b5b7699610253cbb17960a689e32a1712c0b
John Franti & Geoff WInner <student@epicodus.com>
2015-04-09 16:35:23 -0700

    Complete Random result generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,80 @@
 var pitch = function(choice) {
-    var p = choice;
-    var call, random = Math.random();
+    var pitches = choice;
+    var result, random = Math.random();
+    var call = 0;
+    var balls = 0;
+    var strikes = 0;
+    var outs = 0;
+    var runs = 0;
+    var base = 0;
 
-    if (p === 0) {
-        if (random < 0.01) {
-            var call = 1;
-        } else if (random < 0.04) {
-            var call = 2;
-        } else if (random < 0.08) {
-            var call = 3;
-        } else if (random < 0.13) {
-            var call = 4;
-        } else if (random < 0.48) {
-            var call = 5;
-        } else {
-            var call = 6;
+//Pitches and Outcomes
+        if (pitches === 0) {
+            if (random < 0.01) {
+                var call = 1;
+            } else if (random < 0.04) {
+                var call = 2;
+            } else if (random < 0.08) {
+                var call = 3;
+            } else if (random < 0.13) {
+                var call = 4;
+            } else if (random < 0.48) {
+                var call = 5;
+            } else {
+                var call = 6;
+            }
+        }
+        else if (pitches === 1) {
+            if (random < 0.02) {
+                var call = 1;
+            } else if (random < 0.07) {
+                var call = 2;
+            } else if (random < 0.17) {
+                var call = 3;
+            } else if (random < 0.32) {
+                var call = 4;
+            } else {
+                var call = 6;
+            }
+        }
+        else if (pitches === 2) {
+            if (random < 0.01) {
+                var call = 0;
+            } else if (random < 0.02) {
+                var call = 1;
+            } else if (random < 0.05) {
+                var call = 2;
+            } else if (random < 0.09) {
+                var call = 3;
+            } else if (random < 0.14) {
+                var call = 4;
+            } else if (random < 0.50) {
+                var call = 5;
+            } else {
+                var call = 6;
+            }
         }
         return call;
+        console.log(call);
+    };
 
-    else if (p === 1) {
-        if (random < 0.02) {
-            var call = 1;
-        } else if (random < 0.07) {
-            var call = 2;
-        } else if (random < 0.17) {
-            var call = 3;
-        } else if (random < 0.32) {
-            var call = 4;
-        } else {
-            var call = 6;
-        }
-        return call;
-
-    else if (p === 2) {
-        if (random < 0.01) {
-            var call = 0;
-        } else if (random < 0.02) {
-            var call = 1;
-        } else if (random < 0.05) {
-            var call = 2;
-        } else if (random < 0.09) {
-            var call = 3;
-        } else if (random < 0.14) {
-            var call = 4;
-        } else if (random < 0.50) {
-            var call = 5;
-        } else {
-            var call = 6;
-        }
-        return call;
-
-        
-
-    }
+// //Balls and Strikes
+//     for(var c in call) {
+//         if (c === 6) {
+//             strikes++;
+//         } else if (c === 5) {
+//             ball++;
+//         } else if (c === 4) {
+//             base++;
+//         } else if (c === 3) {
+//             base = base + 2;
+//         } else if (c === 2) {
+//             base = base + 4;
+//         } else if (c === 1) {
+//             base = base + 3;
+//         } else if (c === 0) {
+//             base = base++;
+//         }
+//     }
+//     return
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,24 +1,8 @@
-describe('scrabbler', function(){
+describe('pitch', function(){
 
-    it("returns 1 for an entry of the word 'A'.", function(){
-        expect(scrabbler("A")).to.equal(1);
+    it("returns 1 for an entry of ", function(){
+        expect(pitch(0)).to.equal(6);
     });
 
-    it("returns 1 for an entry of the word 'E'.", function(){
-        expect(scrabbler("E")).to.equal(1);
-    });
-
-    it("returns 2 for an entry of the word 'AE'.", function(){
-        expect(scrabbler("AE")).to.equal(2);
-    });
-
-    it("returns 4 for an entry of the word 'F'.", function(){
-        expect(scrabbler("F")).to.equal(4);
-    });
-
-    it("returns 13 for an entry of the word 'fox'.", function(){
-        expect(scrabbler("fox")).to.equal(13);
-    });
-
-
+    
 });
