409e3833d0707c39c6e078e4265ae3bd43aceb48
John Franti & Geoff Winner <student@epicodus.com>
2015-04-29 12:12:35 -0700

    complete simple logic for factory

--- services/BaseballFactory.js
+++ services/BaseballFactory.js
@@ -17,45 +17,52 @@ baseball.factory('BaseballFactory', function BaseballFactory() {
 
     factory.strike = function () {
         factory.strike = (factory.strike + 1);
-    }
+    };
 
     factory.ball = function() {
         factory.ball = (factory.ball + 1);
-    }
+    };
 
     factory.out = function() {
         factory.outs = (factory.outs + 1);
-    }
+    };
 
     factory.single = function() {
         factory.1stbase = 1;
-    }
+    };
 
     factory.double = function() {
         factory.2ndbase = 1;
-    }
+    };
 
     factory.triple = function() {
         factory.3rdbase = 1;
-    }
+    };
 
     factory.homerun = function() {
         factory.1stbase = 0;
         factory.2ndbase = 0;
         factory.2ndbase = 0;
         factory.runs = (factory.runs + 1);
-    }
+    };
 
     factory.advancebaserunners = function() {
         if (factory.3rdbase === 1) {
             factory.home = factory.home + 1;
             factory.3rdbase = 0;
-        } 
-)
-    }
+        };
+        if (factory.2ndbase === 1) {
+            factory.3rdbase = 1;
+            factory.2ndbase = 0;
+        };
+        if (factory.1stbase === 1) {
+            factory.2ndbase = 1;
+            factory.1stbase = 0;
+        };
+    };
 
     factory.endinning = function() {
-
-    }
+        factory.innings = factory.innings + 1;
+    };
 
 });
